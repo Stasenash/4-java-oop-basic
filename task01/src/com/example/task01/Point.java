81236d40a0ebffe20885a9b5c8434bf30df92aee
stasenash <staslisenash@gmail.com>
2020-02-28 11:24:53 +0500

    Update Point.java

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -4,8 +4,8 @@ package com.example.task01;
  * Класс точки на плоскости
  */
 public class Point {
-    int x;
-    int y;
+    private int x;
+    private int y;
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
